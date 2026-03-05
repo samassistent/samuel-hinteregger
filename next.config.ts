94e946a32eac1eaa04aaa86bdf70290318914e6f
Samuel Hinteregger <hintereggerdistribution@gmail.com>
2026-03-05 19:53:08 +0100

    Static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // skip strict mode
+  output: "export",
+  trailingSlash: true,
   reactStrictMode: false,
   env: {
     googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
-  }
+  },
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
